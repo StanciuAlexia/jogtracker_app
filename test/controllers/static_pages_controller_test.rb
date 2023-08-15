d0421ca64ab396c682cf5c197ae0adef6b42fd4d
Alexia_St <stanciu.12ioana@gmail.com>
2023-08-15 09:18:38 +0300

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | JogTracker App"
+    assert_select "title", "JogTracker App"
   end
 
   test "should get help" do
